b4d45f5e56af39677d61b22c9a17386ae4467b83
Adelakun Tobiloba <adelakuntobiloba1@gmail.com>
2023-05-03 05:06:16 +0100

    Initialize files

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,12 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        primary: "#1C9B88",
+        secondary: "#0F574F",
+      },
+      screens: {
+        xs: "440px",
       },
     },
   },
